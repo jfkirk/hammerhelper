ENH: More abilities, units, and fixes

hammerhelper/abilities.py
@@ -47,7 +47,7 @@ class Ability(object):
     def modify_effective_attacks(self, effective_attacks):
         return effective_attacks
 
-    def modify_hit_rolls(self, hit_rolls):
+    def modify_hit_rolls(self, hit_rolls, hit_target):
         return hit_rolls
 
     def modify_wound_rolls(self, wound_rolls):
@@ -94,8 +94,17 @@ class HitModifier(Ability):
         super().__init__(name)
         self.modifier = modifier
 
-    def modify_hit_rolls(self, hit_rolls):
+    def modify_hit_rolls(self, hit_rolls, hit_target):
         return hit_rolls + self.modifier
+
+
+class AutoHit(Ability):
+    offensive = True
+    order = 3  # Go after all other hit mods
+
+    def modify_hit_rolls(self, hit_rolls, hit_target):
+        hit_rolls.fill(hit_target + 1)  # The +1 is just for my sanity
+        return hit_rolls
 
 
 class HitReRollAura(Ability):
@@ -110,11 +119,33 @@ class HitReRollAura(Ability):
     def is_active_prompt(self, attacking_unit, target_unit=None, aura_unit=None):
         if attacking_unit == aura_unit:
             return True
-        return boolean_prompt('Is {} within {}" of {}? '.format(attacking_unit.name, self.aura_range, aura_unit.name))
+        return boolean_prompt('{}: Is {} within {}" of {}? '.format(self.name, attacking_unit.name, self.aura_range, aura_unit.name))
 
-    def modify_hit_rolls(self, hit_rolls):
+    def modify_hit_rolls(self, hit_rolls, hit_target):
         failed_rolls = np.argwhere(hit_rolls <= self.value)
         reroll = np.random.randint(1, 6, size=len(failed_rolls))
         for i, failed_roll_loc in enumerate(failed_rolls):
             hit_rolls[failed_roll_loc] = reroll[i]
         return hit_rolls
+
+
+class WoundReRollAura(Ability):
+    offensive = True
+    aura = True
+
+    def __init__(self, name, value, aura_range):
+        super().__init__(name)
+        self.value = value
+        self.aura_range = aura_range
+
+    def is_active_prompt(self, attacking_unit, target_unit=None, aura_unit=None):
+        if attacking_unit == aura_unit:
+            return True
+        return boolean_prompt('{}: Is {} within {}" of {}? '.format(self.name, attacking_unit.name, self.aura_range, aura_unit.name))
+
+    def modify_wound_rolls(self, wound_rolls):
+        failed_rolls = np.argwhere(wound_rolls <= self.value)
+        reroll = np.random.randint(1, 6, size=len(failed_rolls))
+        for i, failed_roll_loc in enumerate(failed_rolls):
+            wound_rolls[failed_roll_loc] = reroll[i]
+        return wound_rolls

hammerhelper/sim.py
@@ -7,11 +7,17 @@ from units import WeaponType
 N_SIMS = 10000
 
 
+def roll_dice(d, size):
+    return np.random.randint(1, d + 1, size=size)
+
+
 def roll_from_dice_string(dice_string, size):
     if dice_string == 'D3':
-        return np.random.randint(1, 3, size=size)
+        return roll_dice(3, size=size)
     elif dice_string == 'D6':
-        return np.random.randint(1, 6, size=size)
+        return roll_dice(6, size=size)
+    elif dice_string == '2D6':
+        return roll_dice(6, size=size) + roll_dice(6, size=size)
     else:
         raise Exception('Invalid dice string')
 
@@ -102,7 +108,7 @@ def simulate_unit(attacking_unit, weapon, n_shots, target_unit, offensive_abilit
     # Sort abilities on their "order" value
     all_abilities.sort(key=lambda x: x.order)
 
-    relevant_unit_skill = attacking_unit.ws if weapon.weapon_type == WeaponType.MELEE else attacking_unit.bs
+    hit_target_value = attacking_unit.ws if weapon.weapon_type == WeaponType.MELEE else attacking_unit.bs
     t, s, wound_target_value = calculate_wound_target_value(attacking_unit, weapon, target_unit)
     save_target_value = calculate_save_target_value(weapon, target_unit, all_abilities)
 
@@ -111,23 +117,26 @@ def simulate_unit(attacking_unit, weapon, n_shots, target_unit, offensive_abilit
     for sim in range(N_SIMS):
 
         # If applicable, roll for n_attacks
-        if 'D' in str(weapon.effective_attacks):  # TODO don't do this string conversion in the tight loop
-            n_attacks = sum(roll_from_dice_string(weapon.effective_attacks, size=n_shots))
+        weapon_effective_attacks = weapon.effective_attacks(attacking_unit)
+        if 'D' in str(weapon_effective_attacks):  # TODO don't do this string conversion in the tight loop
+            n_attacks = sum(roll_from_dice_string(weapon_effective_attacks, size=n_shots))
         else:
             n_attacks = n_shots
 
         # Roll the attack dice
-        hit_rolls = np.random.randint(1, 6, size=n_attacks)
+        hit_rolls = roll_dice(6, size=n_attacks)
         for ability in all_abilities:
-            hit_rolls = ability.modify_hit_rolls(hit_rolls)
-        n_hits = (hit_rolls >= relevant_unit_skill).sum()
+            hit_rolls = ability.modify_hit_rolls(hit_rolls, hit_target_value)
+        n_hits = (hit_rolls >= hit_target_value).sum()
 
         # Roll the wound dice
-        wound_rolls = np.random.randint(1, 6, size=n_hits)
+        wound_rolls = roll_dice(6, size=n_hits)
+        for ability in all_abilities:
+            wound_rolls = ability.modify_wound_rolls(wound_rolls)
         n_wounds = (wound_rolls >= wound_target_value).sum()
 
         # Roll the save dice
-        save_rolls = np.random.randint(1, 6, size=n_wounds)
+        save_rolls = roll_dice(6, size=n_wounds)
         n_unsaved = n_wounds - (save_rolls >= save_target_value).sum()
 
         # Roll damage
@@ -147,23 +156,31 @@ def simulate_unit(attacking_unit, weapon, n_shots, target_unit, offensive_abilit
     # Unzip results and calculate percentiles
     all_attacks, all_hits, all_wounds, all_damages, all_kills = \
         (list(res) for res in zip(*sims_attacks_hits_wounds_damages_kills))
+    index_5th = int(.05 * N_SIMS)
     index_25th = int(.25 * N_SIMS)
     index_median = int(.5 * N_SIMS)
     index_75th = int(.75 * N_SIMS)
+    index_95th = int(.95 * N_SIMS)
 
     all_attacks.sort()
-    attacks_result = (all_attacks[index_25th], all_attacks[index_median], all_attacks[index_75th])
+    attacks_result = (all_attacks[index_5th], all_attacks[index_25th], all_attacks[index_median],
+                      all_attacks[index_75th], all_attacks[index_95th],)
 
     all_hits.sort()
-    hits_result = (all_hits[index_25th], all_hits[index_median], all_hits[index_75th])
+    hits_result = (all_hits[index_5th], all_hits[index_25th], all_hits[index_median],
+                   all_hits[index_75th], all_hits[index_95th],)
 
     all_wounds.sort()
-    wounds_result = (all_wounds[index_25th], all_wounds[index_median], all_wounds[index_75th])
+    wounds_result = (all_wounds[index_5th], all_wounds[index_25th], all_wounds[index_median],
+                     all_wounds[index_75th], all_wounds[index_95th],)
 
     all_damages.sort()
-    damages_result = (all_damages[index_25th], all_damages[index_median], all_damages[index_75th])
+    damages_result = (all_damages[index_5th], all_damages[index_25th], all_damages[index_median],
+                      all_damages[index_75th], all_damages[index_95th],)
 
     all_kills.sort()
-    kills_result = (all_kills[index_25th], all_kills[index_median], all_kills[index_75th])
+    kills_result = (all_kills[index_5th], all_kills[index_25th], all_kills[index_median],
+                    all_kills[index_75th], all_kills[index_95th],)
 
-    return [attacks_result, hits_result, wounds_result, damages_result, kills_result]
+    return [attacks_result, "{}+".format(hit_target_value), hits_result, "{}+".format(wound_target_value),
+            wounds_result, "{}+".format(save_target_value), damages_result, kills_result]